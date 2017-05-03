aabe4a422669f54a915af201c513f4fab28f36c9
yaobinhuang <815315876@qq.com>
2017-05-03 19:25:30 +0800

    shiyan2

--- yao.php
+++ yao.php
@@ -1,1 +1,30 @@
-<sdfjsdjfsfdksmfik
+<?php
+	header("content-type:text/html;charset=utf-8;");
+	$value='dfkjdf';
+	echo $value."<br/>";
+	
+
+	if (empty($value)) {
+		echo'帅';
+	}else{
+		echo '很帅';
+	}
+	echo '<br>';
+	$name=$value.'<br/>';
+	$yao=&$value.'<br/>';
+	$value="ppppp";
+	echo$name.$yao;
+	
+	$x="3";
+	$y="24";
+	$z=&$x;
+	$x=&$y;
+	$y=&$z;
+	
+
+
+	echo$x.$y;
+	
+	
+	
+ ?>
