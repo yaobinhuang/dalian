afd98bbf38c87c0eed05b2cfb8c6b2fb07b4d045
yaobinhuang <815315876@qq.com>
2017-05-04 11:07:24 +0800

    201754_01

---
+++ zuoye4.27.5.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+<html>
+	<head>
+		<style>
+			#h_title {
+			width: 100%;
+			height:60px;
+			padding-left: 200px;
+			border-top: 1px solid #3D6FB5;
+			clear: both;
+			
+		}
+		#top{width: 1000px;
+			height: 120px;
+			
+			margin: 0px auto;
+
+
+
+		}
+		#h_tu{
+			width:500px;
+			height: 120px;
+			
+			
+			float: left;
+		}
+		#h_tuleft{ 
+			width:500px;
+			height: 120px;
+			
+			float: right;
+		}
+		
+		
+
+
+	
+
+		.a1 {display:block;
+			width: 100px;
+			height: 50px;
+			color: black;
+			font-weight: bolder;
+			font-size: 20px;
+			text-decoration: none;
+			float: left;
+			text-align: center;
+			line-height: 50px;
+		}
+		.a1:hover{
+			background: #3D6FB5;
+			color: white;
+		}
+		.a2{
+			text-decoration: none;
+
+		}
+		#p1{  
+			text-indent: 300px;
+			color: blue;
+				
+		}
+		#input1{    margin-left: 200px;
+			     height: 20px;
+			     border-radius: 0px;
+			     height: 30px;
+			     border: 1px blue solid;
+			     border-right: none;
+		}	
+		#input2{   
+			
+			 border-radius: 0px;	
+			    height: 34px;
+			line-height: 20px;
+			   border: 1px blue solid;
+			   border-left: none;
+			   background: blue;
+			color: white;
+
+		}
+		#top1{
+			width: 100%;
+			min-height: 50px;
+
+		}
+		#body{
+			width: 1126px;
+			min-height: 152px;
+			margin:0px auto;
+			overflow: hidden;
+
+		}
+		#body #body_left{width: 190px;
+			min-height: 200px;
+			
+			float: left;
+			
+
+		}
+		#body #body_left #body_left1{
+			width: 190px;
+			min-height: 70px;
+			background: #6BC4EA;
+
+		}
+		#body #body_left #body_left1 p{
+			width: 80px;
+			height: 50px;
+			border-bottom: 2px solid white;
+			text-align: center;
+			line-height: 50px;
+			font-size: 20px;
+			margin:0px auto;
+			color: white;
+
+		}
+		#body #body_left #body_left2{width: 190px; min-height: 200px;text-align: left;font-size: 15px;}
+		#body #body_left #body_left2 span{font-size: 20px;color: #6BC4EA; }
+		#body #body_right{
+			width: 814px;
+			min-height: 200px;
+			
+			float: right;
+			margin-top: 10px;
+			}
+		#body #body_right #kuag{width: 814px;min-height: 20px;clear: both;}
+		#body #body_right #kuag #right_top1{width: 407px;height: 40px;float: left;border-bottom: 3px solid #6BC4EA;font-size: 20px; color:#6BC4EA; 
+						line-height: 40px;text-indent: 5px; }
+		#body #body_right #kuag #right_top2{width: 407px;height: 41px;float: left; border-bottom: 1px solid #6BC4EA;}
+		#body #body_right .right_body{width: 814px;min-height:150px;clear: both;margin-top: 40px}
+		#body #body_right .right_body .ul1{width: 814px;min-height:150px;margin:0px;padding: 0px;list-style: none;clear: both;margin-top: 10px;}
+		#body #body_right .right_body .ul1 .li1{width: 200px;min-height: 150px;float: left;}
+		#body #body_right .right_body .ul1 .li2{width: 550px;min-height: 150px;float: right; }
+		#body #body_right .right_body .ul1 .li2 .a1_1{text-decoration:none; }
+		#body #body_right .right_body .ul1 .li2 .a2_2{text-decoration: none;display: block;width: 90px;height: 30px;border: 1px solid blue;text-align: center;line-height: 30px;margin-top: 45px;}
+		#body #body_right .right_body .ul1 .li2 .p1_1{text-indent: 20px;}
+		#body #body_right .right_body .ul1 .li2 .p2_2{float: right;margin-top: -140px; margin-right: 10px;}
+	
+		#bottom{width: 100%;min-height: 150px;background: #0FA9EB;margin-top: 50px;color: white;text-align: center;padding-top: 20px;}
+		#bottom a{text-decoration: none;color: white;}
+
+		</style>	
+
+	</head>
+	<body>
+
+		<div id="top">
+			<div id="h_tu"><img src="./image/2017-04-26 11:44:19屏幕截图.png" alt="" /></div>
+			<div  id="h_tuleft">
+				
+				<p id="p1"><a href="" class="a2">设为首页</a>丨<a href="" class="a2">加入收藏</a></p>
+				<input id="input1" type="text" /><input  id="input2" type="submit" value="搜索" />
+
+				
+			</div>
+		</div>	
+		<div id="h_title">
+		    <a  class="a1" href="">网站首页</a>
+		    <a href="" class="a1">关于诚技</a>
+		    <a href="" class="a1">新闻资讯</a>
+		    <a href="" class="a1">产品中心</a>
+		    <a href="" class="a1">成功案例</a>
+		    <a href="" class="a1">人才招聘</a>
+		    <a href="" class="a1">联系我们</a>
+		</div>   
+		<div id="top1"><img src="./image/2017-04-28 09:06:28屏幕截图.png" alt="" /></div>
+		<div id="body">
+			<div id="body_left">
+				<div id="body_left1"><p>成功案例</p></div>
+				<div id="body_left2">
+					<p ><span>联系我们</span></p>
+					<p>电 话：0411-62634264</p>
+					<p>传 真：0411-62634263</p>
+					<p>邮 箱：hr@dlcj.net</p>
+					<p>网 址：http://www.dlcj.net</p>
+					<p>地 址：辽宁省大连市开发区东北七街上品公馆A座1108 </p>
+				</div>
+
+			</div>
+			<div id="body_right">
+				<div id="kuag">
+					<div id="right_top1">成功案例</div>
+					<div id="right_top2"></div>
+				</div>
+			<?php	
+				echo '<div class="right_body">';
+					echo'<ul class="ul1">';
+						echo'<li class="li1"><a href=""><img src="./image/2017-04-27 19:44:37屏幕截图.png" alt="" /></a></li>';
+						echo'<li class="li2">
+							<a href="" class="a1_1">机械压力机大修改造</a>
+							<p class="p1_1">...</p>
+							<a href="" class="a2_2">+查看祥情</a>
+							<p class="p2_2">[ 2016-03-28 ]</p>
+							
+						</li>';
+
+					echo'</ul>';
+					echo'<ul class="ul1">';
+						echo'<li class="li1"><a href=""><img src="./image/2017-04-28 09:09:05屏幕截图.png" alt="" /></a></li>';
+						echo'<li class="li2">
+							<a href="" class="a1_1">机械压力机搬迁</a>
+							<p class="p1_1">...</p>
+							<a href="" class="a2_2">+查看祥情</a>
+							<p class="p2_2">[ 2016-03-28 ]</p>
+							
+						</li>';
+
+					echo'</ul>';
+					echo'<ul class="ul1">';
+						echo'<li class="li1"><a href=""><img src="./image/2017-04-28 09:12:52屏幕截图.png" alt="" /></a></li>';
+						echo'<li class="li2">
+							<a href="" class="a1_1">自动化设计改造</a>
+							<p class="p1_1">...</p>
+							<a href="" class="a2_2">+查看祥情</a>
+							<p class="p2_2">[ 2016-03-28 ]</p>
+							
+						</li>';
+
+					echo'</ul>';
+					echo'<ul class="ul1">';
+						echo'<li class="li1"><a href=""><img src="./image/2017-04-28 09:15:05屏幕截图.png" alt="" /></a></li>';
+						echo'<li class="li2">
+							<a href="" class="a1_1">压力机备件</a>
+							<p class="p1_1">...</p>
+							<a href="" class="a2_2">+查看祥情</a>
+							<p class="p2_2">[ 2016-03-28 ]</p>
+							
+						</li>';
+
+					echo'</ul>';
+					echo '<ul class="ul1">';
+						echo'<li class="li1"><a href=""><img src="./image/2017-04-28 09:16:39屏幕截图.png" alt="" /></a></li>';
+						echo'<li class="li2">
+							<a href="" class="a1_1">平锻机制造及维修</a>
+							<p class="p1_1">...</p>	
+							<a href="" class="a2_2">+查看祥情</a>
+							<p class="p2_2">[ 2016-03-28 ]</p>
+							
+						</li>';
+
+					echo'</ul>';
+					echo'<ul class="ul1">';
+						echo'<li class="li1"><a href=""><img src="./image/2017-04-28 09:21:24屏幕截图.png" alt="" /></a></li>';
+						echo'<li class="li2">
+							<a href="" class="a1_1">热模段的维修和改造</a>
+							<p class="p1_1">...</p>	
+							<a href="" class="a2_2">+查看祥情</a>
+							<p class="p2_2">[ 2016-03-28 ]</p>
+							
+						</li>';
+
+					echo'</ul>';
+				
+				echo'</div>';
+			?>
+			</div>
+
+		</div>
+		
+		<div id="bottom">
+					<a href="">关于诚技</a> | <a href="">新闻资讯</a> | <a href="">产品中心</a> | <a href="">成功案例</a> | <a href="">人才招聘</a> | <a href=""> 联系我们</a>
+					<p>Copyright © 大连诚技机电设备有限公司 版权所有 辽ICP备13015234号-2</p>
+					<p>网站设计：仟亿科技</p>
+ 
+		</div>
+	</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
---
+++ zuoye5.1.1.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+<html>
+	<head>
+		<style>
+			#top{
+				width: 1000px;
+				min-height: 120px;
+				margin:0px auto;				
+			}
+			#top #top_left{
+				width: 500px;
+				min-height: 120px;				
+				float: left;
+			}
+			#top #top_right{
+				width: 500px;
+				min-height: 90px;
+				float: left;
+				text-align: right;
+				padding-top: 30px;
+				color: blue;
+			}
+			#top #top_right a{
+				text-decoration: none;
+				color: blue;
+
+			}
+			#top #top_right #input1{
+				height: 28px;
+				border: 1px solid blue;
+				border-right: none;
+			}
+			#top #top_right #input2{
+				height: 32px;
+				margin-left: -6px;
+				margin-top: 1px;
+				line-height: 1px;
+				border: 1px solid blue;
+				border-left: none;
+				background: blue;
+				color: white;
+			}
+			#title{
+				width: 100%;
+				height: 50px;
+				border-top: 1px solid blue;				
+			}
+			#title #t_help{
+				width: 1000px;
+				height: 50px;				
+				margin:0px auto;
+			}
+			#title  #t_help .a1{
+				display: block;
+				width: 142px;
+				height: 50px;
+				text-align: center;
+				line-height: 50px;
+				text-decoration: none;
+				float: left;
+				font-weight: bolder;
+				font-size: 20px;
+				color: black;
+			}
+			#title  #t_help .a1:hover {
+				color: white;
+				background: #3D6FB5;
+			}
+			#title #title_tu{
+				width: 100%;
+				min-height: 100px;
+			}
+			#body{
+				width: 1000px;
+				min-height: 150px;
+				overflow: hidden;			
+				margin:0px auto;
+			}
+			#body #b_left{
+				width: 190px;
+				min-height: 150px;
+				float: left;				
+			}
+			#body #b_left #left1{
+				width: 190px;
+				height: 70px;
+				background: #6AC2E8;
+
+			}
+			#body #b_left #left1 #border{
+				width: 100px;
+				height: 50px;
+				border-bottom: 1px solid white;
+				text-align: center;
+				line-height: 50px;
+				margin-left: 20px;
+				color: white;
+				font-size: 20px;
+				font-weight: bolder;
+			}
+			#body #b_left .left2{
+				width: 190px;
+				height: 60px;
+				text-indent: 20px;
+				line-height: 60px;
+			}
+			#body #b_left .left2 a{
+				text-decoration: none;
+				color: black;
+				font-size: 17px;
+			}
+			#body #b_left .left2:hover{
+				background: blue;
+			}
+			#body #b_left .left2:hover a{
+				color: white;
+			}
+			#body  #b_right{
+				width: 780px;
+				min-height: 150px;
+				float: right;s
+			
+			}
+			#body  #b_right #right_top{width: 780px;min-height: 20px;clear: both;margin-bottom: 80px;}
+			#body  #b_right #right_top #r_top1{width: 90px;height:50px;border-bottom: 3px solid #10A9EC;text-align: center;line-height: 50px;color: #10A9EC;font-size: 20px;float: left;}
+			#body  #b_right #right_top #r_top2{border-bottom: 1px solid #10A9EC; float: left;width: 690px;height: 51px;}
+			#body  #b_right .ul1{
+				clear: both;
+				width: 780px;
+				min-height: 150px;				
+				margin:0px;
+				padding: 0px;
+				list-style: none;
+				margin-top: 50px;
+			}
+			#body  #b_right .ul1 .li1{
+				width: 190px;
+				min-height: 100px;
+				float: left;
+			}
+			#body  #b_right .ul1 .li2{
+				width: 554px;
+				min-height: 100px;
+				float: right;
+			}
+			#body  #b_right .ul1 .li2 .a1{
+				text-decoration: none;
+				font-size: 18px;
+			}
+			#body  #b_right .ul1 .li2 .a2{
+				display: block;
+				text-decoration: none;
+				width: 80px;
+				height: 20px;
+				border: 1px solid blue;
+			}
+			#body  #b_right .ul1 .li2 .p1{
+				text-align: right;
+				margin-top: -20px;
+			}
+			#body  #b_right .ul1 .li2 .p2{
+				text-indent: 30px;
+				font-size: 14px;
+			}
+			#bottom{width: 100%;min-height: 150px;background: #0FA9EB;margin-top: 50px;color: white;text-align: center;padding-top: 20px;}
+			#bottom a{text-decoration: none;color: white;}
+
+		</style>
+	</head>
+	<body>  
+		<div id="top">
+			<div id="top_left">
+				<img src="./image/2017-04-26 11:44:19屏幕截图.png" alt="" />
+			</div>
+			
+			<div id="top_right">
+				<a href="">设置首页</a>| <a href="">加入收藏</a><br />
+				<input type="text"  placeholder="请输入关键词......"/ id="input1">
+				<input type="submit" value="搜索" / id="input2">	
+			</div>
+		</div>
+		<div id="title">
+			<div id=t_help>
+
+				<a href="" class="a1">网站首页</a>
+				<a href="" class="a1">关于诚技</a>
+				<a href="" class="a1">新闻资讯</a>
+				<a href="" class="a1">产品中心</a>
+				<a href="" class="a1">成功案例</a>
+				<a href="" class="a1">人才招聘</a>
+				<a href="" class="a1">联系我们</a>
+			</div>
+		</div>
+		<div id="title_tu"><img src="./image/2017-05-01 15:27:07屏幕截图.png" alt="" /></div>
+		<div id="body">
+			<div id="b_left">
+				<div id="left1">
+					<div id="border">产品中心</div>
+
+				</div>
+				<div class="left2">
+					<a href="">>公司新闻</a>
+
+				</div>
+				<div class="left2">
+					<a href="">>行业新闻</a>
+
+				</div>
+
+
+			</div>
+			<div id="b_right">
+				<div id="right_top">
+					<div id="r_top1">新闻资讯</div>
+					<div id="r_top2"></div>
+
+				</div>
+			<?php	
+				$value='';
+				$value.='<ul class="ul1">';
+					$value.= '<li class="li1"><a href=""><img src="./image/2017-05-01 16:47:15屏幕截图.png" alt="" /></a></li>';
+					$value.=' <li class="li2">
+						<a href="" class="a1"> 帅呆了！国之「神器」！</a>
+					 	<p class="p1"> [ 2017-04-06 ]</p>
+					 	<p class="p2">    3月18日上午8时38分，中信重工重大技改项目—5000吨自由锻造油压机开始热负荷试生产，与配套的200吨•米操作机同步联动作业，至10时38分成功首锻。       该油压机由德国威普...</p>
+					 	<a href="" class="a2">+ 查看详情</a>
+
+					 </li>';
+			
+				$value.='</ul>';
+				$value.='<ul class="ul1">';
+				$value.=' <li class="li1"><a href=""><img src="./image/2017-05-02 18:57:27屏幕截图.png" alt="" /></a></li>';
+					 $value.= '<li class="li2">
+					 	<a href="" class="a1"> 为什么要造8万吨模锻压机？水平高在哪里？...</a>
+					 	<p class="p1">  [ 2017-04-06 ]</p>
+					 	<p class="p2">    众所周知，被誉为工业皇冠上明珠的航空发动机的制造问题，一直是国人心中的“痛”。我国由于受到欧美国家出口限制，无法造出可靠性高的高性能航空发动机！如今，这个大国重器的出现，将会彻底改变这种现状！中国二重制造的8万吨模锻压机它就是四川德阳的中国二重制造的8万吨模锻压机，这台8万吨级模...</p>
+					 	<a href="" class="a2">+ 查看详情</a>
+
+					 </li>';
+
+				$value.='</ul>';
+				$value.= '<ul class="ul1">';
+					$value.='<li class="li1"><a href=""><img src="./image/2017-05-02 18:58:49屏幕截图.png" alt="" /></a></li>';
+					 $value.= '<li class="li2">
+					 	<a href="" class="a1"> 和谐共赢 携手未来</a>
+					 	<p class="p1"> [ 2017-02-24 ]</p>
+					 	<p class="p2">  祝大连诚技与System Seals中国代理金总洽谈圆满成功！2017年2月23日星期四,System Seals中国代理与大连诚技代表在上品会议室进行第一次洽谈。本次会议为了进一步洽谈代理事宜，以及顺利合作，双方负责人就双...
+					 	</p>
+					 	<a href="" class="a2">+ 查看详情</a>
+
+					 </li>';
+
+				$value.='</ul>';
+				$value.='<ul class="ul1">';
+					$value.=' <li class="li1"><a href=""><img src="./image/2017-05-02 19:00:08屏幕截图.png" alt="" /></a></li>';
+					$value.='  <li class="li2">
+					 	<a href="" class="a1">  不忘初心 不畏前行</a>
+					 	<p class="p1">  [ 2017-01-21 ]</p>
+					 	<p class="p2"> 时光飞逝，岁月如梭，转眼间忙碌的2016年已过去，充满期待的2017年向我们走来。新的一年，孕育新的目标和希望。大连诚技机电设备有限公司 2017年年会于2017年1月18日在九龙湾隆重举行，公司全体员工，公司同仁与公司领导欢聚一堂，共度联欢...</p>
+					 	<a href="" class="a2">+ 查看详情</a>
+
+					 </li>';
+
+				$value.= '</ul>';
+				$value.='<ul class="ul1">';
+					 $value.='<li class="li1"><a href=""><img src="./image/2017-05-02 19:02:23屏幕截图.png" alt="" /></a></li>';
+					 $value.='<li class="li2">
+					 	<a href="" class="a1">   2016年终总结会</a>
+					 	<p class="p1"> [ 2017-04-06 ]</p>
+					 	<p class="p2">    2017年1月18日，我司在上品办公室会议室举行了一年一度的年终总结大会，公司董事长刘彦凯、刘楠经理及干部员工参加大会。会议由刘楠经理主持。    会上首先由各部长分别就本部门全年主要工作完成情况和2017年重点工作安排进...</p>
+					 	<a href="" class="a2">+ 查看详情</a>
+
+					 </li>';
+
+				$value.='</ul>';
+				$value.='<ul class="ul1">';
+					$value.=' <li class="li1"><a href=""><img src="./image/2017-05-02 19:03:25屏幕截图.png" alt="" /></a></li>';
+					$value.='<li class="li2">
+					 	<a href="" class="a1">   当我们还在效仿精益生产时，丰田又推出了T...</a>
+					 	<p class="p1">[ 2016-12-06 ]</p>
+					 	<p class="p2">   天津一汽丰田新第一生产线项目成为了丰田在海外第一个TNGA工厂。新第一生产线项目以“高自动化、高智能化、高效率、注重环保”为目标，项目总占地面积41.69万平方米，建筑面积23.6万平方米，新增投资54.1亿元，新生产线设计产能20万辆/年、一期产能10万辆/年。天津一汽丰田新第...</p>
+					 	<a href="" class="a2">+ 查看详情</a>
+
+					 </li>';
+
+			
+				$value.='</ul>';
+			
+			echo $value;
+			?>
+
+
+			</div>
+		</div>
+		<div id="bottom">
+			<a href="">关于诚技</a> | <a href="">新闻资讯</a> | <a href="">产品中心</a> | <a href="">成功案例</a> | <a href="">人才招聘</a> | <a href=""> 联系我们</a>
+					<p>Copyright © 大连诚技机电设备有限公司 版权所有 辽ICP备13015234号-2</p>
+					<p>网站设计：仟亿科技</p>
+
+
+		</div>		
+		
+		
+	</body>
+</html>
+
+
+
+    
+   
+ 
+
+  
+       
+
+
+   
+   
+        
+    
+  
+  
+     
+
+
+  
+    
+  
+
+  
+   
+       
+    
+  
+  
+    
+   
+    
+   
+    
+
